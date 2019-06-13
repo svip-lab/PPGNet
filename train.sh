66d8cbbaab5fd5a41e386c9184997c7eb1e45b83
Ziheng Zhang <allankevinrichie@users.noreply.github.com>
2019-06-13 10:07:01 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ python main.py \
 --junc-pooling-size 64 --attention-sigma 1.5 --block-inference-size 128 \
 --data-root /data/path --junc-sigma 3 \
 --batch-size 16 --gpus 0,1,2,3 --num-workers 10 --resume-epoch latest \
---is-train-junc True --is-train-adj False \
+--is-train-junc True --is-train-adj True \
 --vis-junc-th 0.1 --vis-line-th 0.1 \
     - train --end-epoch 9 --solver SGD --lr 0.2 --weight-decay 5e-4 --lambda-heatmap 1. --lambda-adj 5. \
     - train --end-epoch 15 --solver SGD --lr 0.02 --weight-decay 5e-4 --lambda-heatmap 1. --lambda-adj 10. \
