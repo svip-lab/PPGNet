e9cc5bd4be969b48a13743b49ae628bef59fb236
Ziheng Zhang <allankevinrichie@users.noreply.github.com>
2019-07-10 08:28:28 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-python main.py \
+python test.py \
 --exp-name line_weighted_wo_focal_junc --backbone resnet50 \
 --backbone-kwargs '{"encoder_weights": "ckpt/backbone/encoder_epoch_20.pth", "decoder_weights": "ckpt/backbone/decoder_epoch_20.pth"}' \
 --dim-embedding 256 --junction-pooling-threshold 0.2 \
